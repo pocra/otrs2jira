6839604e2035ccef68c77875356e54cf0cb102e8
Robert <robbie@pocra.tk>
2020-11-12 11:15:22 +0100

    Update ticket_export.sql

--- ticket_export.sql
+++ ticket_export.sql
@@ -1,9 +1,9 @@
-SELECT CONCAT ('ITS-', t.id+1000) as ticketid, a.id as articleid, t.tn, t.title, t.customer_user_id, ts.name as state, tp.name as priority, t.create_time, t.change_time, u.login AS created, v.login AS changed, adm.a_from, adm.a_subject AS title, REPLACE(adm.a_body, '\n', '\\\\') AS description, CONCAT (t.change_time, ';', v.login, ';', REPLACE(adm.a_body, '\n', '\\\\')) AS comment, CONCAT('file://', att.id, '-', att.filename) AS attachment
+SELECT CONCAT ('ITS-', t.id+1000) as ticketid, art.id as articleid, t.tn AS orts_ticketnumber, t.title, t.customer_user_id, state.name as state, prio.name as priority, t.create_time, t.change_time, u_create.login AS created, u_change.login AS changed, art_mime.a_from, art_mime.a_subject AS title, REPLACE(art_mime.a_body, '\n', '\\\\') AS description, CONCAT (t.change_time, ';', u_change.login, ';', REPLACE(art_mime.a_body, '\n', '\\\\')) AS comment, CONCAT('file://', art_att.id, '-', art_att.filename) AS attachment
 FROM ticket t
-LEFT JOIN article a on t.id = a.ticket_id
-LEFT JOIN article_data_mime adm on a.id = adm.article_id
-LEFT JOIN article_data_mime_attachment att on a.id = att.article_id
-LEFT JOIN ticket_state ts on t.ticket_state_id = ts.id
-LEFT JOIN ticket_priority tp on t.ticket_priority_id = tp.id
-LEFT JOIN users u on t.create_by = u.id
-LEFT JOIN users v on t.change_by = v.id
+LEFT JOIN article art on t.id = art.ticket_id
+LEFT JOIN article_data_mime art_mime on art.id = art_mime.article_id
+LEFT JOIN article_data_mime_attachment art_att on art.id = art_att.article_id
+LEFT JOIN ticket_state state on t.ticket_state_id = state.id
+LEFT JOIN ticket_priority prio on t.ticket_priority_id = prio.id
+LEFT JOIN users u_create on t.create_by = u_create.id
+LEFT JOIN users u_change on t.change_by = u_change.id
